920e5cc32e335c935982caa462393a2bfaf2b8c9
agus supriadi <agus.supriadi.aria@gmail.com>
2024-11-24 12:39:25 +0700

    update

--- src/app/name/page.tsx
+++ src/app/name/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Name() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      testing hahaa
+      name hahaa
     </main>
   );
 }
